219662cea7ec46f3d15ab5f0680b84da3ee17683
Tony3243 <113482331+Tony3243@users.noreply.github.com>
2025-06-25 16:43:39 -0700

    used loguru to add the logs into the detection process for any possible threats

--- src/detector.py
+++ src/detector.py
@@ -4,10 +4,10 @@ import sys
 import re
 from loguru import logger
 
+logger.add(sys.stdout, level='WARNING')
+logger.add('sample_auth.log', level="WARNING")
+
+
 
 with open('logs/sample_auth.log', 'r') as logs:
     samples = logs.readlines()
-
-        
-
-
