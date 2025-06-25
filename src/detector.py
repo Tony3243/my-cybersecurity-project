a6aa9daeed6bd5beb649c261b9bed2edaf652207
Tony3243 <113482331+Tony3243@users.noreply.github.com>
2025-06-25 15:07:52 -0700

    made some importation and started opening files in sample_auth.log

--- src/detector.py
+++ src/detector.py
@@ -1,1 +1,13 @@
 # main detection logic (read and analyzes logs)
+import pandas as pd
+import sys
+import re
+from loguru import logger
+
+
+with open('logs/sample_auth.log', 'r') as logs:
+    samples = logs.readlines()
+
+        
+
+
