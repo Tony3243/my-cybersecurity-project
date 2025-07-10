1c99041021a4d65e112a5bc6a45d79064fa3b6bb
Tony3243 <113482331+Tony3243@users.noreply.github.com>
2025-07-10 14:38:30 -0700

    condensed my parsing syntax by taking awya print as I will use that in the detectors algorithm. Additionally, made sure to make five groups matching each quality in the log including an optional invalid user

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,11 @@
 # Helper functions (e.g., regex patterns, timestamp parser)
 import re
 
-PASSWORD_PATTERN = re.compile(r"\w{3}\s+\d{1,2}\s\d{2,1}:\d{2,1}:\d{2,1}\s(Failed password|Accepted password) for (?:invalid user )?(\w+)(\d{1,3}(?:\.\d{1,3}){3}) ")
+PASSWORD_PATTERN = re.compile(r"(\w{3}\s+\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2}\s)(Failed password|Accepted password) for ((?:invalid user )?(\w+)) from (\d{1,3}(?:\.\d{1,3}){3}) ")
 
-def extracting_potential_anomoly(line):
+def extracting_potential_anomaly(line):
     identical = PASSWORD_PATTERN.search(line)
-    groups = identical.groups(1,2,3,4)
-    if all(groups):
-        print("Time Stamp: ", groups[0])
-        print("Activity: ", groups[1])
-        print("User: ", groups[2])
-        print("IP Address: ", groups[3])
+    if identical:
+        return identical.groups()
     else:
         return None
